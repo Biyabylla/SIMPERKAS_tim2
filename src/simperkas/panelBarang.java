e9114c4ac224ff2bc042605009c5fa5c7e35a5a1
Biyabylla <ardinanabila35@gmail.com>
2025-12-02 13:57:22 +0700

    tambah library

--- src/simperkas/panelBarang.java
+++ src/simperkas/panelBarang.java
@@ -26,19 +26,43 @@ public class panelBarang extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lNamaPerkakas = new javax.swing.JLabel();
+        lPanelBarang = new javax.swing.JLabel();
+
+        lNamaPerkakas.setFont(new java.awt.Font("Segoe UI Black", 1, 12)); // NOI18N
+        lNamaPerkakas.setText("Nama Perkakas      :");
+
+        lPanelBarang.setIcon(new javax.swing.ImageIcon(getClass().getResource("/simperkas/panelBarang.png"))); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(lPanelBarang)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(371, 371, 371)
+                    .addComponent(lNamaPerkakas)
+                    .addContainerGap(372, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(lPanelBarang)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(287, 287, 287)
+                    .addComponent(lNamaPerkakas)
+                    .addContainerGap(287, Short.MAX_VALUE)))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lNamaPerkakas;
+    private javax.swing.JLabel lPanelBarang;
     // End of variables declaration//GEN-END:variables
 }
