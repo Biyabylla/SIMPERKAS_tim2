0f8f9fd6980cec5bc35570e4ac74aef3181832db
Biyabylla <ardinanabila35@gmail.com>
2025-12-16 16:10:02 +0700

    ubah ukuran login

--- src/simperkas/formLogin.java
+++ src/simperkas/formLogin.java
@@ -43,7 +43,7 @@ public class formLogin extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(null);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         tUSERNAME.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         tUSERNAME.addActionListener(new java.awt.event.ActionListener() {
@@ -51,20 +51,17 @@ public class formLogin extends javax.swing.JFrame {
                 tUSERNAMEActionPerformed(evt);
             }
         });
-        getContentPane().add(tUSERNAME);
-        tUSERNAME.setBounds(710, 250, 280, 30);
+        getContentPane().add(tUSERNAME, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 250, 280, 30));
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Password :");
-        getContentPane().add(jLabel3);
-        jLabel3.setBounds(710, 290, 90, 20);
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 290, 90, -1));
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Username :");
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(710, 230, 90, 20);
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 230, 90, -1));
 
         tMASUK.setBackground(new java.awt.Color(0, 51, 153));
         tMASUK.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
@@ -75,8 +72,7 @@ public class formLogin extends javax.swing.JFrame {
                 tMASUKActionPerformed(evt);
             }
         });
-        getContentPane().add(tMASUK);
-        tMASUK.setBounds(710, 360, 280, 40);
+        getContentPane().add(tMASUK, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 360, 280, 40));
 
         tRESET.setBackground(new java.awt.Color(204, 0, 51));
         tRESET.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
@@ -87,14 +83,12 @@ public class formLogin extends javax.swing.JFrame {
                 tRESETActionPerformed(evt);
             }
         });
-        getContentPane().add(tRESET);
-        tRESET.setBounds(910, 410, 75, 23);
-        getContentPane().add(tPASSWORD);
-        tPASSWORD.setBounds(710, 310, 280, 30);
+        getContentPane().add(tRESET, new org.netbeans.lib.awtextra.AbsoluteConstraints(910, 410, 75, -1));
+        getContentPane().add(tPASSWORD, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 310, 280, 30));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/simperkas/SIMPERKAS NW.gif"))); // NOI18N
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(0, 0, 1100, 600);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pict/SIMPERKAS NW.gif"))); // NOI18N
+        jLabel1.setPreferredSize(new java.awt.Dimension(1000, 600));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1100, 600));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
